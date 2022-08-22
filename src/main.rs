d914244a6980f082598fe3d92836010d0906bebe
LeeTaeHwa <driversivese@gmail.com>
2022-08-22 19:47:14 +0900

    load and spell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod tsl;
+
+/* 
+    std library repository
+    -> https://github.com/rust-lang/rust/tree/master/library/std/src
+
+    Official document
+    -> https://doc.rust-lang.org/beta/test/index.html
+*/
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/tsl/dm/mod.rs
@@ -0,0 +1,9 @@
+
+/*
+    DM = Dark Magic.
+
+    Any things inner operation system are strongly forbbiden to programmers.
+    But some progreammers are willing to take a risk and try dark magic.
+*/
+
+pub mod elements;
